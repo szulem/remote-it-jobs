ca5b6438ca8ef59e27f5db9aca6fc9d85e4370b3
szulem <mirek@szul.eu>
2019-04-23 18:23:37 +0200

    Add category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_23_153919) do
+ActiveRecord::Schema.define(version: 2019_04_23_162242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "jobs", force: :cascade do |t|
     t.string "title"
@@ -26,6 +32,9 @@ ActiveRecord::Schema.define(version: 2019_04_23_153919) do
     t.string "currency"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_jobs_on_category_id"
   end
 
+  add_foreign_key "jobs", "categories"
 end
