62eb1b471839832b1fef90ad3aef9e3aca93655f
szulem <mirek@szul.eu>
2019-04-23 18:19:01 +0200

    Add model and controller of jobs

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,17 @@
+class JobsController < ApplicationController
+  def index
+  	@jobs = Job.all
+  end
+
+  def show
+  	@job = Job.find(params[:id])
+  end
+
+  def new
+  	@job = Job.new
+  end
+
+  def edit
+  	@job = Job.find(params[:id])
+  end
+end
