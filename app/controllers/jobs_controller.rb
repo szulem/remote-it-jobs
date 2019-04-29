6f816fe7252dacbc9762b35f29edea0d0fee629c
szulem <mirek@szul.eu>
2019-04-29 14:09:24 +0200

    Add find_job method

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,50 +1,60 @@
 class JobsController < ApplicationController
-	before_action :authenticate_user!, except: %i[index show]
-	before_action :set_all_categories, only: [:new, :edit]
+  before_action :authenticate_user!, except: %i[index show]
+  before_action :job_owner, only: %i[edit update destroy]
+  before_action :set_all_categories, only: [:new, :edit]
+  before_action :find_job, only: %i[show edit update job_owner]
 
   def index
-  	@jobs = Job.all.order('created_at DESC')
+    @jobs = Job.all.order('created_at DESC')
   end
 
   def show
-  	@job = Job.find(params[:id])
-  	@jobs = Job.all.order('created_at DESC').take(5)
+    @jobs = Job.all.order('created_at DESC').take(5)
   end
 
   def new
-  	@job = Job.new
+    @job = Job.new
   end
 
   def create
-  	@job = Job.new(job_params)
-  	if @job.save
-  		redirect_to @job, notice: "Successfully added a new Job"
-  	else
-  		render 'new'
-  	end
+    @job = current_user.jobs.new(job_params)
+    if @job.save
+      redirect_to @job, notice: "Successfully added a new Job"
+    else
+      render 'new'
+    end
   end
 
   def edit
-  	@job = Job.find(params[:id])
   end
 
   def update
-  	@job = Job.find(params[:id])
-  	if @job.update(job_params)
-  		redirect_to @job, notice: "The job was successfully updated."
-  	else
-  		render 'edit'
-  	end
+    if @job.update(job_params)
+      redirect_to @job, notice: "The job was successfully updated."
+    else
+      render 'edit'
+    end
   end
 
   private
 
   def job_params
-  	# params.require(:job).permit(:title, :category_id)
-  	params.require(:job).permit(:title, :description, :email, :url, :salary_from, :salary_to, :salary_per, :currency, :category_id, :company_name, :company_logo)
+    # params.require(:job).permit(:title, :category_id)
+    params.require(:job).permit(:title, :description, :email, :url, :salary_from, :salary_to, :salary_per, :currency, :category_id, :company_name, :company_logo, :user_id)
+  end
+
+  def find_job
+    @job = Job.find(params[:id])
   end
 
   def set_all_categories
-  	@categories = Category.all
+    @categories = Category.all
+  end
+
+  def job_owner
+    unless @job.user_id == current_user.id
+      flash[:alert] = "You don't have permission to edit this offer."
+      redirect_to root_path
+    end
   end
 end
