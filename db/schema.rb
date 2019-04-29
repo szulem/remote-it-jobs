8bf2478c34578147bd80da797b93e22f7f509587
szulem <mirek@szul.eu>
2019-04-29 14:10:15 +0200

    Add user id to jobs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_28_091545) do
+ActiveRecord::Schema.define(version: 2019_04_28_224723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,7 +38,9 @@ ActiveRecord::Schema.define(version: 2019_04_28_091545) do
     t.bigint "company_logo_file_size"
     t.datetime "company_logo_updated_at"
     t.string "company_name"
+    t.bigint "user_id"
     t.index ["category_id"], name: "index_jobs_on_category_id"
+    t.index ["user_id"], name: "index_jobs_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -54,4 +56,5 @@ ActiveRecord::Schema.define(version: 2019_04_28_091545) do
   end
 
   add_foreign_key "jobs", "categories"
+  add_foreign_key "jobs", "users"
 end
