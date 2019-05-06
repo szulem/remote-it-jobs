2f8ab40b639130cb8d111b72eac20544345e0247
szulem <mirek@szul.eu>
2019-05-07 00:09:56 +0200

    Create users controller and display user's jobs

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   root 'jobs#index'
   resources :jobs
   resources :categories, param: :name
+  resources :users
+  # get '/users/:id' => 'users#show', as: :profile
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
