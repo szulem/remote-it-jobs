cf34b8b5513ad89348fe3a068ca5b17d3aef9c40
szulem <mirek@szul.eu>
2019-05-19 21:47:08 +0200

    Fixes with admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: %i[show]
+
   def show
     @user = User.find(params[:id])
     @jobs = @user.jobs.order('created_at DESC')
+    @jobs_all = Job.all.order('created_at DESC')
+
+    if @user != current_user
+      flash[:error] = "You don't have permission to display this page."
+      redirect_to root_path
+    end
+
   end
 end
