3fdadd5d7b4019305fcd35c5a4803a840a5ffac9
szulem <mirek@szul.eu>
2019-07-09 17:57:58 +0200

    Changed to 14 days while the job is new

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,7 +1,7 @@
 module JobsHelper
 
   def new_offer?(offer)
-    if offer.created_at >= 7.day.ago
+    if offer.created_at >= 14.day.ago
       # image_tag("new_icon.png", alt: "new job offer")
       "<div class='new-offer'>new</div>".html_safe
     end
