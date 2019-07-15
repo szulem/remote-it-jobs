7ed6daf9afc8455b24a3102bc9337c714fcd2e87
szulem <mirek@szul.eu>
2019-07-15 17:53:47 +0200

    Changed the mailer url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,7 +105,7 @@ Rails.application.configure do
     }
   }
 
-  config.action_mailer.default_url_options = { host: 'remojobs.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'remojobs.com' }
 
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
