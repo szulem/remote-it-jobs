615043a271bce7871c159d32ca8bd43ea50e6ee6
szulem <mirek@szul.eu>
2019-08-30 08:49:54 +0200

    Changed production smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,7 +112,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-    :address              => "msmedia.atthost24.pl",
+    :address              => "smtp.gmail.com",
     :port                 => 587,
     :user_name            => ENV['gmail_username'],
     :password             => ENV['gmail_password'],
