d7af4b8e246ec474a04d47464c6c808f8ed159ab
szulem <mirek@szul.eu>
2023-02-15 14:07:06 +0100

    Add mailjet config and credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,17 +112,7 @@ Rails.application.configure do
   #   }
   # }
 
-  config.action_mailer.default_url_options = { host: 'remojobs.com' }
+  # config.action_mailer.default_url_options = { host: 'remojobs.com' }
 
-  config.action_mailer.delivery_method = :smtp
-  # SMTP settings for gmail
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :user_name            => ENV['gmail_username'],
-    :password             => ENV['gmail_password'],
-    :authentication       => "plain",
-    :enable_starttls_auto => true
-  }
-
+  config.action_mailer.delivery_method = :mailjet_api
 end
