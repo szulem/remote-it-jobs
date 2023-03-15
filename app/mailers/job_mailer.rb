5784bd385d66332f3ca886a214eb0c208879a11f
Mirosław Szul <mirek@szul.eu>
2023-03-15 23:57:26 +0100

    Change mailer sender name

--- app/mailers/job_mailer.rb
+++ app/mailers/job_mailer.rb
@@ -1,6 +1,6 @@
 class JobMailer < ApplicationMailer
   def new_job_send(job)
     @job = job
-    mail to: @job.user.email, subject: "Your job offer for #{@job.title} has been activated!", from: "REMOJOBS.com <hello@remojobs.com>"
+    mail to: @job.user.email, subject: "Your job offer for #{@job.title} has been activated!", from: "REMOJOBS <hello@remojobs.com>"
   end
 end
