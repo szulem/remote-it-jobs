de8f0af85d261278de67e1c8a5f5d31a8322381b
Mirosław Szul <mirek@szul.eu>
2023-03-21 23:38:43 +0100

    Remove unnecessary code

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -2,7 +2,6 @@ module JobsHelper
 
   def new_offer?(offer)
     if offer.created_at >= 14.day.ago
-      # image_tag("new_icon.png", alt: "new job offer")
       "<div class='new-offer'>new</div>".html_safe
     end
   end
