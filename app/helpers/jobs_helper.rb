40f9e3341762d38856ec5f8e77f94a18601cb489
Mirosław Szul <mirek@szul.eu>
2023-03-26 14:36:53 +0200

    Change div to span i new offer

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -2,7 +2,7 @@ module JobsHelper
 
   def new_offer?(offer)
     if offer.created_at >= 14.day.ago
-      "<div class='new-offer'>new</div>".html_safe
+      "<span class='new-offer'>new</span>".html_safe
     end
   end
 
